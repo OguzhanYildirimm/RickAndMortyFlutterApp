2db236e89de2ca2e17f7fcf38257473e90d3936c
OguzhanYildirimm <oguzhany1337@gmail.com>
2023-06-10 20:47:21 +0300

    character details screen added

--- lib/character_details_page.dart
+++ lib/character_details_page.dart
@@ -1,52 +1,155 @@
 import 'dart:io';
 
+import 'package:demo_application/episode_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'model.dart';
 
 class CharacterDetails extends StatefulWidget {
-  const CharacterDetails({super.key, required this.characterId});
+  CharacterDetails({super.key, required this.characterModel});
 
   @override
   State<CharacterDetails> createState() => _CharacterDetailsState();
 
-  final String characterId;
+  Results characterModel = Results();
 }
 
 class _CharacterDetailsState extends State<CharacterDetails> {
-  Results? _characterDetail;
-  Dio networkManager =
-      Dio(BaseOptions(baseUrl: "https://rickandmortyapi.com/api/character/"));
+  Dio networkManager = Dio();
+  List<EpisodeModel>? _episodeModelList;
   bool isLoading = false;
 
   void isLoadingCircular() {
-    setState(() {
-      isLoading = !isLoading;
-    });
+    isLoading = !isLoading;
   }
 
-  Future<void> fetchData() async {
+  Future<void> getEpisodes(List<String> episodeUrl) async {
     isLoadingCircular();
-    final response = await networkManager.get(widget.characterId);
-    if (response.statusCode == HttpStatus.ok) {
-      final characterDetail = response.data;
-      _characterDetail = Results.fromJson(characterDetail);
+    List<EpisodeModel> episodeModelList = [];
+
+    for (String url in episodeUrl) {
+      final response = await networkManager.get(url);
+
+      if (response.statusCode == HttpStatus.ok) {
+        EpisodeModel episodeModel = EpisodeModel.fromJson(response.data);
+        episodeModelList.add(episodeModel);
+      }
     }
+    setState(() {
+      _episodeModelList = episodeModelList;
+    });
     isLoadingCircular();
   }
 
   @override
   void initState() {
     super.initState();
-    fetchData();
+    getEpisodes(widget.characterModel.episode ?? []);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_characterDetail?.name.toString() ?? ""),
+        title: Text(
+          widget.characterModel.name ?? "",
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall
+              ?.copyWith(color: Colors.white, fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              _CharacterImage(
+                widget: widget,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(widget.characterModel.gender ?? ""),
+                    Text(widget.characterModel.status ?? ""),
+                    Text(
+                      widget.characterModel.origin?.name ?? "",
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(widget.characterModel.gender ?? "")
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              isLoading
+                  ? const CircularProgressIndicator.adaptive()
+                  : _CharacterAppearsInEpisode(
+                      episodeModelList: _episodeModelList),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CharacterAppearsInEpisode extends StatelessWidget {
+  const _CharacterAppearsInEpisode({
+    required List<EpisodeModel>? episodeModelList,
+  }) : _episodeModelList = episodeModelList;
+
+  final List<EpisodeModel>? _episodeModelList;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: GridView.builder(
+        scrollDirection: Axis.horizontal,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: _episodeModelList?.length ?? 0,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+              elevation: 5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(_episodeModelList?[index].episode ?? ""),
+                ],
+              ));
+        },
+      ),
+    );
+  }
+}
+
+class _CharacterImage extends StatelessWidget {
+  const _CharacterImage({
+    required this.widget,
+  });
+
+  final CharacterDetails widget;
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: widget.characterModel.name ?? "",
+      child: Container(
+        height: 150,
+        width: 150,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                image: NetworkImage(widget.characterModel.image ?? ""))),
       ),
     );
   }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,10 +13,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Results>? _characters;
+  final TextEditingController _controller = TextEditingController();
+  List<Results> filterCharacterList = [];
+  List<Results>? _exampleCharacters;
+  List<Results>? _allCharacters;
+
   Dio networkManager =
       Dio(BaseOptions(baseUrl: "https://rickandmortyapi.com/api/"));
+
   bool isLoading = false;
+  bool isDownloading = false;
 
   void isLoadingCircular() {
     setState(() {
@@ -28,18 +34,52 @@ class _HomePageState extends State<HomePage> {
     isLoadingCircular();
     final response = await networkManager.get('character');
     if (response.statusCode == HttpStatus.ok) {
-      final characterList = response.data["results"];
-      if (characterList is List) {
-        _characters = characterList.map((e) => Results.fromJson(e)).toList();
+      final charactersList = response.data["results"];
+      if (charactersList is List) {
+        _exampleCharacters =
+            charactersList.map((e) => Results.fromJson(e)).toList();
       }
     }
     isLoadingCircular();
+  }
+
+  Future<void> fetchDataWithNamed() async {
+    List<Results> charactersList = [];
+    final response = await networkManager.get('character');
+    if (response.statusCode == HttpStatus.ok) {
+      final responseData = response.data["info"];
+      Info info = Info.fromJson(responseData);
+      int? count = info.count;
+      for (int i = 1; i <= (count ?? 0); i++) {
+        final response = await networkManager.get('character/$i');
+        if (response.statusCode == HttpStatus.ok) {
+          Results characterModel = Results.fromJson(response.data);
+          charactersList.add(characterModel);
+        }
+      }
+    }
+    setState(() {
+      isDownloading = !isDownloading;
+      _allCharacters = charactersList;
+    });
+  }
+
+  void filter(String name) {
+    if (_allCharacters != null) {
+      setState(() {
+        filterCharacterList = _allCharacters!
+            .where(
+                (data) => data.name!.toLowerCase().contains(name.toLowerCase()))
+            .toList();
+      });
+    }
   }
 
   @override
   void initState() {
     super.initState();
     fetchData();
+    fetchDataWithNamed();
   }
 
   @override
@@ -61,37 +101,98 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemCount: _characters?.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => CharacterDetails(
-                      characterId: _characters?[index].id.toString() ?? "",
+        child: Column(
+          children: [
+            SizedBox(
+              height: 75,
+              child: TextField(
+                keyboardType: TextInputType.name,
+                enabled: isDownloading,
+                controller: _controller,
+                onChanged: (value) => filter(value),
+                decoration: InputDecoration(
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepOrangeAccent)),
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepOrangeAccent)),
+                    border: const OutlineInputBorder(),
+                    hintText: isDownloading
+                        ? "Now you can search for characters !"
+                        : "Please Wait ..."),
+              ),
+            ),
+            _controller.text.isEmpty
+                ? Expanded(
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemCount: _exampleCharacters?.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => CharacterDetails(
+                                  characterModel: Results(
+                                      name: _exampleCharacters?[index].name,
+                                      status: _exampleCharacters?[index].status,
+                                      image: _exampleCharacters?[index].image,
+                                      gender: _exampleCharacters?[index].gender,
+                                      origin: _exampleCharacters?[index].origin,
+                                      episode:
+                                          _exampleCharacters?[index].episode),
+                                ),
+                              ));
+                            },
+                            child: _exampleCharactersCardWidget(
+                                index: index, characters: _exampleCharacters));
+                      },
                     ),
-                  ));
-                },
-                child: _CharactersCardWidget(
-                    index: index, characters: _characters));
-          },
+                  )
+                : Expanded(
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemCount: filterCharacterList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => CharacterDetails(
+                                  characterModel: Results(
+                                      name: filterCharacterList[index].name,
+                                      status: filterCharacterList[index].status,
+                                      image: filterCharacterList[index].image,
+                                      gender: filterCharacterList[index].gender,
+                                      origin: filterCharacterList[index].origin,
+                                      episode:
+                                          filterCharacterList[index].episode),
+                                ),
+                              ));
+                            },
+                            child: _exampleCharactersCardWidget(
+                                index: index, characters: filterCharacterList));
+                      },
+                    ),
+                  ),
+          ],
         ),
       ),
     );
   }
 }
 
-class _CharactersCardWidget extends StatelessWidget {
-  const _CharactersCardWidget({
+class _exampleCharactersCardWidget extends StatelessWidget {
+  const _exampleCharactersCardWidget({
     required int index,
     required List<Results>? characters,
-  })  : _characters = characters,
+  })  : _exampleCharacters = characters,
         _index = index;
 
-  final List<Results>? _characters;
+  final List<Results>? _exampleCharacters;
   final int _index;
 
   @override
@@ -101,34 +202,66 @@ class _CharactersCardWidget extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            height: 75,
-            width: 75,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    image: NetworkImage(_characters?[_index].image ?? ""))),
+          Hero(
+            tag: _exampleCharacters?[_index].name ?? "",
+            child: Container(
+              height: 75,
+              width: 75,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          _exampleCharacters?[_index].image ?? ""))),
+            ),
           ),
           Text(
-            _characters?[_index].name ?? "",
+            _exampleCharacters?[_index].name ?? "",
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.titleMedium?.copyWith(
                 color: Colors.deepOrangeAccent, fontWeight: FontWeight.w800),
           ),
-          Text(
-            _characters?[_index].gender ?? "",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                color: Colors.deepOrangeAccent, fontWeight: FontWeight.w400),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_exampleCharacters?[_index].gender == "Male")
+                const Icon(Icons.male_outlined),
+              if (_exampleCharacters?[_index].gender == "Female")
+                const Icon(Icons.female_outlined),
+              if (_exampleCharacters?[_index].gender == "unknown")
+                const Icon(Icons.question_mark_outlined),
+              Text(
+                _exampleCharacters?[_index].gender ?? "",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Colors.deepOrangeAccent,
+                    fontWeight: FontWeight.w400),
+              ),
+            ],
           ),
-          Text(
-            _characters?[_index].status ?? "",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                color: Colors.deepOrangeAccent,
-                fontWeight: FontWeight.w200,
-                fontStyle: FontStyle.italic),
-          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_exampleCharacters?[_index].status == "Alive")
+                SizedBox(
+                    height: 15,
+                    child: Image.asset('assets/icons/greendot.png')),
+              if (_exampleCharacters?[_index].status == "Dead")
+                SizedBox(
+                    height: 15, child: Image.asset('assets/icons/reddot.png')),
+              if (_exampleCharacters?[_index].status == "unknown")
+                SizedBox(
+                    height: 15,
+                    child: Image.asset('assets/icons/blackdot.png')),
+              Text(
+                _exampleCharacters?[_index].status ?? "",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                    color: Colors.deepOrangeAccent,
+                    fontWeight: FontWeight.w200,
+                    fontStyle: FontStyle.italic),
+              ),
+            ],
+          )
         ],
       ),
     );
